dc0e4a133f9d87fb32c12742673cb2e7e720747a
Abhijeet Malamkar <abhijeetmalamkar@gmail.com>
2021-02-24 21:31:00 -0500

    updated

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-import color from './src/color'
-import time from './src/time'
+// import color from './src/color'
+// import time from './src/time'
 
 
-module.exports.color = color
-module.exports.time = color
+// exports.color = color
+// exports.time = time
+class Color {
+    constructor(name, code) {
+        this.name = name;
+        this.code = code;
+    }
+}
+
+const allColors = [
+    new Color('brightred', '#E74C3C'),
+    new Color('soothingpurple', '#9B59B6'),
+    new Color('skyblue', '#5DADE2'),
+    new Color('leafygreen', '#48C9B0'),
+    new Color('sunkissedyellow', '#F4D03F'),
+    new Color('groovygray', '#D7DBDD'),
+];
+
+exports.getRandomColor = () => {
+    return allColors[Math.floor(Math.random() * allColors.length)];
+}
+
+exports.allColors = allColors;
